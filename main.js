ab8cb234351deeba8a5757630928f87266e9de49
aidanbeck <aidanmbeck@outlook.com>
2025-08-14 16:07:14 -0500

    allow default statements without symbols

--- main.js
+++ main.js
@@ -28,12 +28,22 @@ lines = lines.filter(line => line.length != 0); //remove empty lines
 function getSymbolIndexes(string) {
     let symbolIndexes = [];
 
+    let startsWithoutSymbol = true;
+
     for (let index = 0; index < string.length; index++) { // for each character
+
+        
+
         for (rule of syntax) {                            // for each rule
             if (string.charAt(index) == rule.symbol && string.charAt(index-1) != "\\") { //if the character == the rule's symbol (and isn't ignored)
                 symbolIndexes.push({key: rule.key, index: index});  // push the rule's key and symbol's index
+                startsWithoutSymbol = false;
             }
         }
+    }
+
+    if (startsWithoutSymbol) {
+        symbolIndexes.unshift([{key: "default", index:0}]);
     }
 
     return symbolIndexes;
