ea8e0765a914ded2a3001cfd4bf6ca4a046328b1
aidanbeck <aidanmbeck@outlook.com>
2025-08-14 15:48:40 -0500

    break lines into statements

--- main.js
+++ main.js
@@ -3,10 +3,67 @@ const fs = require('fs');
 //fs.existsSync('input.txt');
 let input = fs.readFileSync('input.txt','utf-8');
 
-let lines = input.split(/\r?\n/);
 
-lines = lines
-    .map(line => line.trim())
-    .filter(line => line.length != 0);
+const syntax = [
+    {symbol:'#', key: 'roomKey'},
+    {symbol:"%", key: 'locationKey'},
+    {symbol:"~", key: 'alterationKey'},
+    {symbol:"*", key: 'prompt'},
+    {symbol:">", key: 'pathKey'},
 
-fs.writeFileSync(`output.json`, JSON.stringify(lines));
+
+    {symbol:"default", key:"paragraphs"},
+];
+
+
+let lines = input.split(/\r?\n/); //split array into lines
+lines = lines.map(line => line.trim()) //remove whitespace
+lines = lines.filter(line => line.length != 0); //remove empty lines
+
+function getSymbolIndexes(string) {
+    let symbolIndexes = [];
+
+    for (let index = 0; index < string.length; index++) { // for each character
+        for (rule of syntax) {                            // for each rule
+            if (string.charAt(index) == rule.symbol && string.charAt(index-1) != "\\") { //if the character == the rule's symbol (and isn't ignored)
+                symbolIndexes.push({key: rule.key, index: index});  // push the rule's key and symbol's index
+            }
+        }
+    }
+
+    return symbolIndexes;
+}
+
+function getLineStatements(string) {
+
+    let statements = [];
+    
+    let symbolIndexes = getSymbolIndexes(string);
+    
+    for (let i = 0; i < symbolIndexes.length; i++) {
+        
+        let start = symbolIndexes[i].index;
+        let end = string.length;
+
+        if (i+1 != symbolIndexes.length) {
+            end = symbolIndexes[i+1].index;
+        }
+
+        let statement = string.slice(start, end);
+
+        statements.push(statement);
+    }
+
+    return statements;
+}
+
+let output = [];
+
+for (let line of lines) {
+    let statements = getLineStatements(line);
+    output.push(statements);
+}
+
+console.log(output);
+
+fs.writeFileSync(`output.json`, JSON.stringify(output));
